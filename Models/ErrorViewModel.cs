6ac3cc4ab97629eb6d3e240f5cd35bd35923072c
Phuc <phucpd17702@gmail.com>
2024-10-17 18:43:56 +0700

    Create_Project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PhamDangPhuc516.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
